1da4924e5afdacad5681539e76d96d515252163c
ravenusmc <mcuddy77@gmail.com>
2022-07-21 16:08:02 -0400

    minor change

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="p-5 text-center bg-image rounded-3 jumbotron-image">
-      <div class="mask" style="background-color: rgba(0, 0, 0, 0.6)">
+      <div class="mask">
         <div class="d-flex justify-content-center align-items-center h-100">
           <div class="text-white">
             <h1 class="mb-3">Examining Dresses</h1>
@@ -30,5 +30,9 @@ export default {
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
+}
+
+.mask {
+  background-color: rgba(0, 0, 0, 0.6);
 }
 </style>
