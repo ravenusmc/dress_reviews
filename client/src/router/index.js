18d8ca86a0d98578335232c487392cc3f1f64af7
ravenusmc <mcuddy77@gmail.com>
2022-07-27 16:30:41 -0400

    working on setting up main page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -16,6 +16,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/main',
+    name: 'main',
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Main.vue')
   }
 ]
 
