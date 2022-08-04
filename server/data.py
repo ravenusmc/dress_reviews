f47c254781ac77b56bb61bf8b901427af489919f
ravenusmc <mcuddy77@gmail.com>
2022-08-04 14:14:18 -0400

    setting up python file for data analysis

---
+++ server/data.py
@@ -0,0 +1,8 @@
+# This file will deal with a lot of the data analysis for this project.
+
+# importing supporting libraries
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+class Data():
