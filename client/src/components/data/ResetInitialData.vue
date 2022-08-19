4ca04636a29d2ecd61b4123132ae67e1f79a96d4
ravenusmc <mcuddy77@gmail.com>
2022-08-19 15:10:03 -0400

    added feature to reset to initial data

--- client/src/components/data/ResetInitialData.vue
+++ client/src/components/data/ResetInitialData.vue
@@ -1,35 +1,39 @@
 <template>
-	<div>
-		<div class='button-div'>
-			<button @click="resetData()" type="button" class="btn btn-dark">Reset To Initial Data</button>
-		</div>
-	</div>
+  <div>
+    <div class="button-div">
+      <button @click="resetData()" type="button" class="btn btn-dark">
+        Reset To Initial Data
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
 export default {
-	name: "ResetInitialData",
-	methods: {
+  name: "ResetInitialData",
+  computed: {
+    ...mapGetters("data", [
+      "firstValue",
+      "lastValue",
+    ]),
+  },
+  methods: {
+    ...mapActions("data", ["fetchInitialTableData"]),
     resetData() {
-			console.log('Hi')
-      // let selection = this.Name;
-      // const payload = {
-      //   selection,
-      //   column: "class_name",
-      //   firstValue: this.firstValue,
-      //   lastValue: this.lastValue,
-      // };
-      // this.changeDataBasedOnSingleSelection({ payload });
+      const payload = {
+        firstValue: this.firstValue,
+        lastValue: this.lastValue,
+      };
+      this.fetchInitialTableData({ payload });
     },
   },
-}
+};
 </script>
 
 <style scoped>
-
 .button-div {
-	margin: 10px 0 10px 0;
-	text-align: center;
+  margin: 10px 0 10px 0;
+  text-align: center;
 }
-
 </style>
