db6c573566545ab9943c6722441c79bda1e1d689
ravenusmc <mcuddy77@gmail.com>
2022-08-31 16:22:39 -0400

    working on building range form

--- client/src/components/data/InputDataRange.vue
+++ client/src/components/data/InputDataRange.vue
@@ -1,11 +1,38 @@
 <template>
   <div>
     <form @submit="submitDataRange">
-      <label for="firstValue">First Value:</label>
-      <input type="number" v-model="firstValue" name="firstValue" />
-      <label for="secondValue">Second Value:</label>
-      <input type="number" v-model="secondValue" name="secondValue" />
-      <button class="btn btn-success">Submit</button>
+      <p>{{ firstValue }}</p>
+      <p>{{ lastValue }}</p>
+      <button @click="changeRange('up')" type="button" class="btn btn-secondary">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-arrow-up"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
+          />
+        </svg>
+      </button>
+      <button @click="changeRange('down')" type="button" class="btn btn-secondary">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-arrow-down"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+          />
+        </svg>
+      </button>
     </form>
   </div>
 </template>
@@ -17,16 +44,26 @@ export default {
   name: "",
   data() {
     return {
-      firstValue: 0,
-      secondValue: 20,
+      // firstValue: 0,
+      // secondValue: 20,
     };
+  },
+  computed: {
+    ...mapGetters("data", [
+      "tableData",
+      "firstValue",
+      "lastValue",
+      "divisionName",
+      "departmentName",
+      "className",
+    ]),
   },
   methods: {
     ...mapActions("data", ["fetchDataBasedOnAge"]),
     submitDataRange(evt) {
       evt.preventDefault();
       let firstValue = Number(this.firstValue);
-			let secondValue = Number(this.secondValue);
+      let secondValue = Number(this.secondValue);
       // const payload = {
       //   tableData: this.tableData,
       //   ageOne,
@@ -39,6 +76,10 @@ export default {
       // };
       // this.fetchDataBasedOnAge({ payload });
     },
+    changeRange(direction) {
+      console.log('HI')
+      console.log(direction)
+    }
   },
 };
 </script>
