1f2f992ff22b6d9bb93e198c971d37a7365a2419
ravenusmc <mcuddy77@gmail.com>
2022-10-01 10:09:41 -0400

    added sentiment graph

--- client/src/components/Graphs/SentimentGraph.vue
+++ client/src/components/Graphs/SentimentGraph.vue
@@ -1,19 +1,40 @@
 <template>
-	<div>	
-		<!-- <form @submit="submitSelectedAges">
-      <label for="rating">Pleae sele:</label>
-      <input
-        type="number"
-        v-model="rating"
-        name="rating"
-        min="1"
-        max="5"
-      /> -->
-	</div>
+  <div>
+    <GChart :type="typeOne" :data="sentiment" :options="chartOptionsOne" />
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import GraphCard from "@/components/Graphs/GraphCard.vue";
+
 export default {
   name: "SentimentCard",
+  components: {
+    GraphCard,
+  },
+  computed: {
+    ...mapGetters("text", ["sentiment"]),
+  },
+  data() {
+    return {
+      typeOne: "ColumnChart",
+      chartOptionsOne: {
+        title: "Sentiment by Rating",
+        legend: { position: "top" },
+        colors: ["#f24867"],
+        height: 500,
+        animation: {
+          duration: 1000,
+          easing: "linear",
+        },
+        vAxis: {
+          viewWindow: {
+            min: 0,
+          },
+        },
+      },
+    };
+  },
 };
 </script>
