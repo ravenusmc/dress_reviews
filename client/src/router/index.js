942ac788d9d2584f57e9c803c86d8a6406df0854
ravenusmc <mcuddy77@gmail.com>
2022-10-29 09:16:42 -0400

    made it so only authorized users can go on certain routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import store from '@/store/index';
+import store from '@/store/index';
 import Landing from '../views/Landing.vue'
 
 Vue.use(VueRouter)
@@ -19,12 +19,40 @@ const routes = [
   {
     path: '/main',
     name: 'main',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Main.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Main.vue'),
+    beforeEnter: (to, from, next) => {
+      if (store.state.common.loginFlag === false) {
+        next('/login');
+      } else {
+        next();
+      }
+    },
+    beforeRouteLeave: (to, from, next) => {
+      if (store.state.common.loginFlag === false) {
+        next('/login');
+      } else {
+        next();
+      }
+    },
   },
   {
     path: '/graphs',
     name: 'graphs',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Graphs.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Graphs.vue'),
+    beforeEnter: (to, from, next) => {
+      if (store.state.common.loginFlag === false) {
+        next('/login');
+      } else {
+        next();
+      }
+    },
+    beforeRouteLeave: (to, from, next) => {
+      if (store.state.common.loginFlag === false) {
+        next('/login');
+      } else {
+        next();
+      }
+    },
   },
   {
     path: '/sign_up',
